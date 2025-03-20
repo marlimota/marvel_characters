01a43a17c9449aafa442dbde1f2b34e6562ef883
Marli Mota <marli.mota42@gmail.com>
2025-03-20 19:45:14 -0300

    feat: add character entity in domain layer

---
+++ lib/domain/entities/character_entity.dart
@@ -0,0 +1,11 @@
+class CharacterEntity {
+  final String name;
+  final String image;
+  final String biography;
+
+  const CharacterEntity({
+    required this.name,
+    required this.image,
+    required this.biography,
+  });
+}
